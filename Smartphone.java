6314460a505b1dfbb873aa970a7cda2e73871f64
Soufian123 <proni10102002@gmail.com>
2023-09-27 16:17:21 +0200

    creacio la classe Smartphone

---
+++ Smartphone.java
@@ -0,0 +1,54 @@
+class Smartphone extends Dispositu{
+    private String sistemaOperatiu;
+    private String hardware;
+    private boolean accelerometre = false;
+    private boolean gps = true;
+
+
+    public Smartphone(String sistemaOperatiu, String hardware){
+        setSistemaOperatiu(sistemaOperatiu);
+        setHardware(hardware);
+    }
+    public Smartphone(String sistemaOperatiu, String hardware, boolean accelerometre){
+        setSistemaOperatiu(sistemaOperatiu);
+        setHardware(hardware);
+        setAccelerometre(accelerometre);
+    }
+
+    public Smartphone(String sistemaOperatiu, String hardware, boolean accelerometre,boolean gps){
+        setSistemaOperatiu(sistemaOperatiu);
+        setHardware(hardware);
+        setAccelerometre(accelerometre);
+        setGps(gps);
+    }
+
+    public void setSistemaOperatiu(String sistemaOperatiu){
+        this.sistemaOperatiu=sistemaOperatiu;
+    }
+    public String getSistemaOperatiu(String sistemaOperatiu){
+        return this.sistemaOperatiu;
+    }
+    public void setHardware(String hardware){
+        this.hardware=hardware;
+    }
+    public String getHardware(String hardware){
+        return this.hardware;
+    }
+    public void setAccelerometre(boolean accelerometre){
+        this.accelerometre=accelerometre;
+    }
+    public boolean getAccelerometre(boolean accelerometre){
+        return this.accelerometre;
+    }
+    public void setGps(boolean gps){
+        this.gps=gps;
+    }
+    public boolean getGps(boolean gps){
+        return this.gps;
+    }
+    @Override
+    public String toString() {
+        String text = String.format("\n  Marca: %s\n Model: %s\n Sistema Operatiu: %s\n    Hardware: %s\n    Accelerometre: %s\n    GPS: %s\n  Preu: %s\n ",getMarca(), getModel(),getSistemaOperatiu(),getHardware(),getAccelerometre(),getGps(),getPreuBase());
+        return text;
+    }
+}
