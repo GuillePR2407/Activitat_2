b7bb906d3ca53c08938027819c3cb7c78de28157
Soufian123 <proni10102002@gmail.com>
2023-09-29 14:53:18 +0200

    fet el main

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Crear 5 dispositivos
-        Dispositiu rellotgeIntelligent = new Dispositiu("Rellotge Intel·ligent", "Xiaomi", 100);
+        Dispositiu rellotgeIntelligent = new AltreDispositiu("Rellotge Intel·ligent", "Xiaomi", 100);
         Smartphone mobilAndroid = new Smartphone("OnePlus", "nord2", 300, "Android", "Hardware2", false, false);
         Smartphone iPhone = new Smartphone("iPhone", "19 PRO MAX", 500, "iOS", "Hardware3", true, true);
         Tablet tablet = new Tablet("Tablet", "Modelo4", 400, 10.1);
